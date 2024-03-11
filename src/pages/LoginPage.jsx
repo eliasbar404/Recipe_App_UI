aa6482ff8afdc7e24fe7435551d65fc605de796f
Sofyane <sofyanessaki@gmail.com>
2024-03-11 18:31:08 +0000

    styling regesterPage and loginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,29 @@
-
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
 
 const LoginPage = () => {
   return (
-    <form>
-      <input type="email" name="" id="" />
-      <input type="password" name="" id="" />
-      <button type="submit">Login</button>
-    </form>
+    <div className="px-16 pt-8 mx-[500px] mt-16 h-[500px]">
+      <div className="flex justify-center my-6 ">
+        <h1 className="text-3xl font-bold mb-2">Log in</h1>  
+      </div>
+      <div className="grid grid-cols-1 content-center my-4">
+        <div className="grid grid-cols-1 gap-2 justify-items-center">
+          <label className="text-lg">Email</label>
+          <input type="email" className="w-64 rounded-l-lg p-2 shadow-xl bg-transparent" placeholder="email"/>
+        </div>
+        <div className="grid grid-cols-1 gap-2 my-8 justify-items-center">
+          <label className="text-lg">Password</label>
+          <input type="password" className="w-64 rounded-l-lg p-2 shadow-xl bg-transparent" placeholder="password"/>
+        </div>
+      </div>
+    <div className="flex justify-center">
+      <Button>Log in</Button>
+    </div>
+    <div className="flex justify-center mt-6">
+        <Link to={'/register'} className="text-sm hover:text-blue-500">create account</Link>
+      </div>
+    </div>
   )
 }
 
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,8 +1,43 @@
-
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className="px-16 pt-8 mx-64 mt-16 h-[500px] ">
+      <div className="my-4">
+        <h1 className="text-3xl">Logo</h1>
+        <div className="my-4 py-2">
+          <h1 className="text-3xl font-bold mb-2">Register</h1>
+          <p className="text-md text-slate-400">Enter your details to create your account and enjoy our services.</p>  
+        </div>
+      </div>
+      <form >
+        <div className="grid grid-cols-2 my-6">
+          <div className="grid grid-cols-1 my-2 ">
+            <label className="text-lg mb-2">Full name</label>
+            <input type="text" className="w-64 rounded-l-lg p-2 shadow-xl bg-transparent" placeholder="name"/>
+          </div>
+          <div className="grid grid-cols-1 my-2 ml-28">
+            <label className="text-lg mb-2">Email</label>
+            <input type="email" className="w-64 rounded-l-lg p-2 shadow-xl bg-transparent" placeholder="email"/>
+          </div>
+          <div className="grid grid-cols-1 my-2 ">
+            <label className="text-lg mb-2">Password</label>
+            <input type="password" className="w-64 rounded-l-lg p-2 shadow-xl bg-transparent" placeholder="password"/>
+          </div>
+          <div className="grid grid-cols-1 my-2 ml-28">
+            <label className="text-lg mb-2">Password confirmation</label>
+            <input type="password" className="w-64 rounded-l-lg p-2 shadow-xl bg-transparent" placeholder="confirmation"/>
+          </div>
+        </div>
+        <div className="flex justify-center mt-16">
+          <Button>Confirm</Button>
+        </div>
+      </form>
+      <div className="flex justify-center mt-6">
+        <Link to={'/login'} className="text-sm hover:text-blue-500">I have already an account</Link>
+      </div>
+    </div>
   )
 }
 
